801590dade3dd5b019e82d839ab6285389a88edf
Iris Cheung <hokyinirischeung@gmail.com>
2025-11-04 09:23:33 -0800

    create model, to_dict, from_dict, completed create and get all task routes, passed couple of wave 1 tests

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,29 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True, default=None)
+
+    def to_dict(self):
+        task_as_dict={}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        if self.completed_at:
+            task_as_dict["is_complete"] = True
+        elif not self.completed_at or self.completed_at == None:
+            task_as_dict["is_complete"] = False
+        return task_as_dict
+    
+    @classmethod
+    def from_dict(cls, task_data):
+        new_task = cls(title=task_data["title"],
+                    description=task_data["description"],
+                    completed_at=None)
+        
+        return new_task
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,48 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort, make_response, Response
+from app.models.task import Task
+from ..db import db
+
+bp = Blueprint("tasks_bp",  __name__, url_prefix="/tasks")
+
+@bp.post("")
+def create_task():
+    request_body = request.get_json()
+    try:
+        new_task = Task.from_dict(request_body)
+    except KeyError as error:
+        response = {"message": f"Invalid request: missing {error.args[0]}"}
+        abort(make_response(response, 400))
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return new_task.to_dict(), 201
+
+@bp.get("")
+def get_all_task():
+    query = db.select(Task)
+    
+    title_param = request.args.get("title")
+    if title_param:
+        query = query.where(Task.title.ilike(f"%{title_param}%"))
+    
+    description_param = request.args.get("description")
+    if description_param:
+        query = query.where(Task.description.ilike(f"%{description_param}%"))
+    
+    complete_param = request.args.get("is_completed")
+    if complete_param:
+        query = query.where(Task.completed_at == None)
+    
+    tasks = db.session.scalars(query.order_by(Task.id))
+
+    tasks_response = []
+    for task in tasks:
+        tasks_response.append(task.to_dict())
+    return tasks_response
+
+@bp.get()
+
+    
+
+    
